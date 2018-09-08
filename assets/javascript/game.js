ff20e38c5acbd27545ae4c7b2eb9a6b92031eef0
Charlie <charlie.islas@gmail.com>
2018-09-08 15:06:16 -0500

    uploading final version with proper format

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //https://github.com/clsavino/PokemonHangmanGame/blob/master/assets/javascript/game.js link to the Pokemon example game
 
-window.onload=function(){
+window.onload = function () {
 
     //making my feline list and declaring its properties, including species names and pictures
     var felines = [
@@ -91,58 +91,58 @@ window.onload=function(){
 
     //Declaring initial variables
 
-    userGuess="";
-    var wins=0;
-    var defeats=0;
-    var catNumber=0;
-    var gameOver=false;
-    var usedLetters=[];
-    var wrongLetters=[];
-    var opportunities=0;
+    userGuess = "";
+    var wins = 0;
+    var defeats = 0;
+    var catNumber = 0;
+    var gameOver = false;
+    var usedLetters = [];
+    var wrongLetters = [];
+    var opportunities = 0;
 
-    document.onkeyup=function(event){
+    document.onkeyup = function (event) {
 
         writeBlanks();
-    
+
     }
-    
-    function writeBlanks(){
 
-        if(catNumber<13){
+    function writeBlanks() {
 
-        var displayArray=[];
+        if (catNumber < 13) {
+
+            var displayArray = [];
 
 
-            currentCat=[];
-            displayedCat="";
-            wordDisplay=[];
-            usedLetters=[];
-            opportunities=7;
+            currentCat = [];
+            displayedCat = "";
+            wordDisplay = [];
+            usedLetters = [];
+            opportunities = 7;
 
             $("#startOrGuess").html("Guess that cat species now!");
             $("#guessOrType").html("Type the letters you guess, but be careful... too many mistakes will lead to your doom!!!");
 
-            currentCat=felines[catNumber].species;
-            console.log("currentCat: "+currentCat);
-        
-            for(var i=0;i<currentCat.length;i++){
+            currentCat = felines[catNumber].species;
+            console.log("currentCat: " + currentCat);
 
-                wordDisplay[i]='_';
-    
+            for (var i = 0; i < currentCat.length; i++) {
+
+                wordDisplay[i] = '_';
+
             }
 
-            displayedCat=wordDisplay.join(" ");
+            displayedCat = wordDisplay.join(" ");
 
             $("#guessedWord").html(displayedCat);
             $("#used").html("");
 
-            $("#catImage").attr("src",felines[catNumber].pic);
+            $("#catImage").attr("src", felines[catNumber].pic);
 
-        keepPlaying();
-    
+            keepPlaying();
+
         }
 
-        else{
+        else {
 
             $("#startOrGuess").html("Congratulations! You've reached the end of the game...");
             $("#guessOrType").html("Come back to the Serengeti any time!");
@@ -153,126 +153,126 @@ window.onload=function(){
 
     }
 
-    function keepPlaying(){
+    function keepPlaying() {
 
-        opportunities=7;
+        opportunities = 7;
         $("#oppsLeft").html(opportunities);
 
-        var wordDisplay=[];
-        for(var i=0;i<currentCat.length;i++){
+        var wordDisplay = [];
+        for (var i = 0; i < currentCat.length; i++) {
 
-            wordDisplay[i]='_';
+            wordDisplay[i] = '_';
 
         }
 
-        displayedCat=wordDisplay.join(" ");
-        
-        $("#guessedWord").html(displayedCat+" ");
-       
-        var lettersLeft=currentCat.length;
-        var addLetter=true;
-        var usedLettersStr="";
+        displayedCat = wordDisplay.join(" ");
 
-        document.onkeyup=function(event){
-     
-            var correct=false;
-            var currentCat=[];
-            var displayedCat=[];
-            var guess=event.key.toLowerCase();  
-            var wrongLetter=true;
-            addLetter=true;
+        $("#guessedWord").html(displayedCat + " ");
 
-            currentCat=felines[catNumber].species;
-            
+        var lettersLeft = currentCat.length;
+        var addLetter = true;
+        var usedLettersStr = "";
 
-            var arrayIndex=usedLetters.indexOf(guess);
-            
+        document.onkeyup = function (event) {
 
-            for(var i=0;i<currentCat.length;i++){
-                
-                arrayIndex=usedLetters.indexOf(guess);
+            var correct = false;
+            var currentCat = [];
+            var displayedCat = [];
+            var guess = event.key.toLowerCase();
+            var wrongLetter = true;
+            addLetter = true;
 
-                    if(currentCat[i]===guess&&arrayIndex===-1){
-
-                        usedLetters.push(guess);
-                        usedLettersStr=usedLettersStr+guess+", ";
-                        $("#used").html(usedLettersStr);
-                        
-                        for(var j=0;j<currentCat.length;j++){
-
-                            if(currentCat[j]===guess){
-                               
-                                wordDisplay[j]=guess;
-                                displayedCat=wordDisplay.join(" ");
-                                $("#guessedWord").html(displayedCat+" ");
-                                lettersLeft--;
-                            }
-
-                        }
-
-                        if(lettersLeft===0){
-
-                            wins++;
-                            $("#victs").html(wins);
-                            catNumber++;
-                            $("#startOrGuess").html("You win! Well done, you are saved from my vicious claws!");
-                            $("#guessOrType").html("Press F to guess another species!");
-
-                            document.onkeyup=function(event){
-
-                                var another=event.key.toLowerCase();
-                                if(another==='f'){
-
-                                    writeBlanks();
-                                                 }
-                                                            }
-                                            }
+            currentCat = felines[catNumber].species;
 
 
-                                                            }
-    
-                                }
-                    
-            for(var i=0;i<currentCat.length;i++){
+            var arrayIndex = usedLetters.indexOf(guess);
 
-                arrayIndex=usedLetters.indexOf(guess);
 
-                if(currentCat[i]!==guess&&arrayIndex===-1){
+            for (var i = 0; i < currentCat.length; i++) {
+
+                arrayIndex = usedLetters.indexOf(guess);
+
+                if (currentCat[i] === guess && arrayIndex === -1) {
 
                     usedLetters.push(guess);
-                    usedLettersStr=usedLettersStr+guess+", ";
+                    usedLettersStr = usedLettersStr + guess + ", ";
+                    $("#used").html(usedLettersStr);
+
+                    for (var j = 0; j < currentCat.length; j++) {
+
+                        if (currentCat[j] === guess) {
+
+                            wordDisplay[j] = guess;
+                            displayedCat = wordDisplay.join(" ");
+                            $("#guessedWord").html(displayedCat + " ");
+                            lettersLeft--;
+                        }
+
+                    }
+
+                    if (lettersLeft === 0) {
+
+                        wins++;
+                        $("#victs").html(wins);
+                        catNumber++;
+                        $("#startOrGuess").html("You win! Well done, you are saved from my vicious claws!");
+                        $("#guessOrType").html("Press F to guess another species!");
+
+                        document.onkeyup = function (event) {
+
+                            var another = event.key.toLowerCase();
+                            if (another === 'f') {
+
+                                writeBlanks();
+                            }
+                        }
+                    }
+
+
+                }
+
+            }
+
+            for (var i = 0; i < currentCat.length; i++) {
+
+                arrayIndex = usedLetters.indexOf(guess);
+
+                if (currentCat[i] !== guess && arrayIndex === -1) {
+
+                    usedLetters.push(guess);
+                    usedLettersStr = usedLettersStr + guess + ", ";
                     $("#used").html(usedLettersStr);
                     opportunities--;
                     $("#oppsLeft").html(opportunities);
 
-                    if(opportunities===0){
+                    if (opportunities === 0) {
                         defeats++;
                         $("#losses").html(defeats);
                         catNumber++;
                         $("#startOrGuess").html("You lose! Be devoured by the Roman lions!");
                         $("#guessOrType").html("Press F to guess another species!");
                         $("#guessedWord").html(currentCat);
-                        document.onkeyup=function(event){
+                        document.onkeyup = function (event) {
 
-                            var another=event.key.toLowerCase();
-                            if(another==='f'){
+                            var another = event.key.toLowerCase();
+                            if (another === 'f') {
 
                                 writeBlanks();
-                                             }
-                                                        }
+                            }
+                        }
 
                     }
 
                 }
 
 
-            }     
+            }
 
-                    }
+        }
 
 
 
-                                            }
+    }
 
 
 
